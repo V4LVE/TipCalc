454f1f264219ea3efb868d7e0768454a908b0311
Alexander Colmorn <Alex802c@gmail.com>
2025-09-03 11:23:57 +0200

    added popsups

--- TipCalc/MainPage.xaml.cs
+++ TipCalc/MainPage.xaml.cs
@@ -27,15 +27,20 @@
             CalculateTip();
         }
 
-        void On15PercentClicked(object sender, EventArgs e)
+        async void On15PercentClicked(object sender, EventArgs e)
         {
+            await DisplayAlert("Alert", "You have tipped 15%", "OK");
             tipPercentage = 15;
             TipSlider.Value = tipPercentage;
             CalculateTip();
         }
 
-        void On20PercentClicked(object sender, EventArgs e)
+        async void On20PercentClicked(object sender, EventArgs e)
         {
+            bool answer = await DisplayAlert("Confirm", "Are you sure you want to tip 20%?", "Yes", "No");
+            if (!answer)
+                return;
+
             tipPercentage = 20;
             TipSlider.Value = tipPercentage;
             CalculateTip();
@@ -81,6 +86,32 @@
 
             TipLabel.Text = $"Tip: {tip:0.00} kr.";
             TotalLabel.Text = $"Total: {total:0.00} kr.";
+        }
+
+        async void OnViewCurrencyClicked(object sender, EventArgs e)
+        {
+            string action = await DisplayActionSheet("Currency", "Cancel", null, "Danske Kroner", "Euro", "Dollars");
+
+            var tip = bill * tipPercentage / 100;
+            var total = bill + tip;
+
+            switch (action)
+            {
+                case "Danske Kroner":
+                    TipLabel.Text = $"Tip: {tip:0.00} kr.";
+                    TotalLabel.Text = $"Total: {total:0.00} kr.";
+                    break;
+                case "Euro":
+                    TipLabel.Text = $"Tip: {tip:0.00} €";
+                    TotalLabel.Text = $"Total: {total:0.00} €";
+                    break;
+                case "Dollars":
+                    TipLabel.Text = $"Tip: {tip:0.00} $";
+                    TotalLabel.Text = $"Total: {total:0.00} $";
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
